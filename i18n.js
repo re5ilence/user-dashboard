84f5922e274764f085733d9cb5faf195b2acfc6a
Vadim Baiusov <feeltransmision@mail.ru>
2025-05-06 18:11:26 +0700

    Добавлено переключение языка

---
+++ i18n.js
@@ -0,0 +1,20 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translationEN from './locales/en/translation.json';
+import translationRU from './locales/ru/translation.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: translationEN },
+      ru: { translation: translationRU },
+    },
+    lng: 'en', 
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -1,45 +1,61 @@
 import { useState, useEffect } from 'react'
 import Button from '../Button/Button'
 import './Settings.css'
+import { useTranslation } from 'react-i18next'
 
 export default function Settings() {
-    const [language, setLanguage] = useState('English');
+    const { t, i18n } = useTranslation();
+    // const [language, setLanguage] = useState('English');
     const [theme, setTheme] = useState(() => {
-        return localStorage.getItem('theme') || 'Night';
+        return localStorage.getItem('theme') || 'night'; // теперь ключ
     });
-    const [notifications, setNotification] = useState('Yes');
+    const [notifications, setNotification] = useState('yes'); // теперь ключ
 
     useEffect(() => {
-        document.body.classList.toggle('day-theme', theme === 'Day');
-        document.body.classList.toggle('night-theme', theme === 'Night');
+        document.body.classList.toggle('day-theme', theme === 'day');
+        document.body.classList.toggle('night-theme', theme === 'night');
         localStorage.setItem('theme', theme);
     }, [theme]);
 
+    // const toggleLanguage = () => {
+    //     setLanguage(prevLanguage => (prevLanguage === 'Russian' ? 'English' : 'Russian'));
+    // };
     const toggleLanguage = () => {
-        setLanguage(prevLanguage => (prevLanguage === 'Russian' ? 'English' : 'Russian'));
+        const newLang = i18n.language === 'ru' ? 'en' : 'ru';
+        i18n.changeLanguage(newLang);
     };
 
     const toggleTheme = () => {
-        setTheme(prevTheme => (prevTheme === 'Day' ? 'Night' : 'Day'));
+        setTheme(prev => (prev === 'day' ? 'night' : 'day'));
     };
 
     const toggleNotification = () => {
-        setNotification(prevNotification => (prevNotification === 'Yes' ? 'No' : 'Yes'));
+        setNotification(prev => (prev === 'yes' ? 'no' : 'yes'));
     };
 
+    // const settingsButtons = [
+    //     { onClick: toggleLanguage, label: language },
+    //     { onClick: toggleTheme, label: theme },
+    //     { onClick: toggleNotification, label: notifications }
+    // ]
+
     const settingsButtons = [
-        { onClick: toggleLanguage, label: language },
-        { onClick: toggleTheme, label: theme },
-        { onClick: toggleNotification, label: notifications }
-    ]
+        { onClick: toggleLanguage, label: t(i18n.language === 'ru' ? 'russian' : 'english') },
+        { onClick: toggleTheme, label: t(theme) },
+        { onClick: toggleNotification, label: t(notifications) }
+    ];
+
 
     return (
         <div className='settings'>
             <div className='row-settings'>
                 <div className="settings-left">
-                    <h2>Language</h2>
+                    {/* <h2>Language</h2>
                     <h2>Theme</h2>
-                    <h2>Notification</h2>
+                    <h2>Notification</h2> */}
+                    <h2>{t('language')}</h2>
+                    <h2>{t('theme')}</h2>
+                    <h2>{t('notification')}</h2>
                 </div>
                 <div className="settings-right">
                     {settingsButtons.map((item, index) => (
